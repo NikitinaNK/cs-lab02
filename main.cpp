5c271c40fd3bf327783188fe4737fed862e6eee5
Alice(NikitinaNK) <iiam-nikitina@icloud.com>
2021-03-15 14:46:12 +0300

    code : max

--- main.cpp
+++ main.cpp
@@ -10,5 +10,9 @@ int main()
     cout<< "A - B = " << a - b << '\n';
     cout << "A * B = " << a * b << '\n';
     cout << "A/B = " << a / b << '\n';
+    if (a > b)
+        cout << a << '\n';
+    else
+        cout << b << '\n';
     return 0;
 }
